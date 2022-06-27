7dee0c24adb89df0cbb4adca52aec4064a828274
mankic <miic0803@gmail.com>
2022-06-27 16:38:30 +0900

    수정 - 5번문제까지

--- post/serializers.py
+++ post/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 # from user.models import User as UserModel
 from post.models import Company as CompanyModel
 from post.models import JobPost as JobPostModel
+from post.models import JobType as JobTypeModel
+from post.models import SkillSet as SkillSetModel
 from post.models import JobPostSkillSet as JobPostSkillSetModel
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -12,14 +14,36 @@ class CompanySerializer(serializers.ModelSerializer):
         fields = ["company_name", "business_area"]
 
 
+class JobTypeSerializer(serializers.ModelSerializer):
+                
+    class Meta:
+        model = JobTypeModel
+        fields = ["job_type"]
+
+
 class JobPostSerializer(serializers.ModelSerializer):
-    company = CompanySerializer()
+    company = CompanySerializer(read_only=True)
+    # job_type = JobTypeSerializer()
+    skillset = serializers.SerializerMethodField()
+
+    def get_skillset(self, obj):
+        skillsets = obj.skillset_set.all()
+        skillset_names = [skillset.name for skillset in skillsets]
+        return skillset_names
+
     class Meta:
         model = JobPostModel
-        fields = ["job_type", "company","job_description","salary"]
+        fields = ["job_type", "company", "job_description", "salary", "skillset"]
+
+
+class SkillSetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillSetModel
+        fields = ["name", "job_posts"]
 
 
 class JobPostSkillSetSerializer(serializers.ModelSerializer):
+    skill_set = SkillSetSerializer()
     job_post = JobPostSerializer()
     class Meta:
         model = JobPostSkillSetModel
--- post/views.py
+++ post/views.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions, status
+
+from post.serializers import JobPostSerializer
 from .models import (
     JobPostSkillSet,
     JobType,
     JobPost,
-    Company
+    Company,
+    SkillSet
 )
 from django.db.models.query_utils import Q
 
@@ -18,7 +21,14 @@ class SkillView(APIView):
         skills = self.request.query_params.getlist('skills', '')
         print("skills = ", end=""), print(skills)
 
-        return Response(status=status.HTTP_200_OK)
+        query = Q(skill_set_id=3) | Q(skill_set_id=2)
+
+        jobpostskillset_objs = JobPostSkillSet.objects.filter(query)
+        jobpost_ids = [jobpostskillset_obj.job_post_id for jobpostskillset_obj in jobpostskillset_objs]
+        
+        jobpost_objs = [JobPost.objects.get(id=jobpost_id) for jobpost_id in jobpost_ids]
+       
+        return Response(JobPostSerializer(jobpost_objs, many=True).data, status=status.HTTP_200_OK)
 
 
 class JobView(APIView):
@@ -26,10 +36,28 @@ class JobView(APIView):
     def post(self, request):
         job_type = int( request.data.get("job_type", None) )
         company_name = request.data.get("company_name", None)
+     
         job_type_table = JobType.objects.filter(id=job_type)
+        # print(job_type_table)
+        company_get_create = Company.objects.get_or_create(company_name=company_name)
+        # print(type(company_get_create)) => <class 'tuple'>     # (<Company: Company object (14)>, True)
+        company_obj = company_get_create[0]  
+        # print(company_obj)
+        # print(type(company_obj))
+        
         if not job_type_table.exists():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({"massage":"job type 이 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(job_type, status=status.HTTP_200_OK)
+        # if not company.exists():
+        #     company = Company(company_name=company_name).save()
+        # else:
+        #     company = company.first()
+
+        job_serializer = JobPostSerializer(data=request.data)
+        if job_serializer.is_valid():
+            job_serializer.save(company=company_obj)
+            return Response({"message":"저장완료!!"}, status=status.HTTP_200_OK)
+
+        return Response(job_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
